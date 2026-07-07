3f4af4322f57f32f41f6d852add9fcda3641586e
seyi-adeoti <oluwaseyi.personaluse@gmail.com>
2026-07-07 12:41:12 +0100

    font-fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
   },
 };
 
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
